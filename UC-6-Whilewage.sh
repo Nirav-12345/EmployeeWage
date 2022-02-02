35f8ce2187e0d1a57ab81fb3e633b336edb55db2
Nirav-12345 <niravpandit0002@gmail.com>
2022-02-01 22:43:23 -0800

    added uc6 file

---
+++ UC-6-Whilewage.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -x
+
+
+
+isPartTime=1
+isFullTime=2
+maxRateInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+
+
+while [[ $totalEmpHr -lt $maxRateInMonth &&
+
+	$totalWorkingDays -lt $numOfWorkingDays ]]
+
+do
+
+		((totalWorkingDays++))
+
+		randomCheck=$((RANDOM%3))
+
+
+
+		case $randomCheck in
+
+					$isFullTime)
+
+							emphrs=8
+
+						;;
+
+
+					$isPartTime)
+
+
+							emphrs=4
+
+
+						;;
+
+
+
+					*)
+
+
+							emphrs=0
+
+						;;
+
+
+			esac
+
+
+
+		totalEmpHr=$(($totalEmpHr+$emphrs))
+
+done
+
+
+			totalSalary=$(($totalEmpHr*$empRatePerHr))
+
+
